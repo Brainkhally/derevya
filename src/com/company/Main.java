51c3fcab68837c85c472075e44a050b48c1261b7
student <student@itstep1.dp.ua>
2020-11-13 11:55:02 +0200

    Деревья

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Tree myTree=new Tree();
+        Scanner scanner=new Scanner(System.in);
+        for (int  i=0;i<3;i++)
+        {
+            System.out.println("enter the value: ");
+            myTree.add(scanner.nextInt(), i);
+        }
+
+        System.out.println(myTree.getRoot());
+        System.out.println(myTree.checkKey(0));
+    }
+}
